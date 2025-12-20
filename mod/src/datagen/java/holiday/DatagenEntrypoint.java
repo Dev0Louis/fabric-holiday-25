4ee9ac63a32fc49d0d4b071b39a1a4afd4abcc5b
haykam821 <24855774+haykam821@users.noreply.github.com>
2025-12-19 21:50:55 -0500

    Use data generation for painting variants

---
+++ mod/src/datagen/java/holiday/DatagenEntrypoint.java
@@ -0,0 +1,20 @@
+package holiday;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
+
+public class DatagenEntrypoint implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator dataGenerator) {
+        FabricDataGenerator.Pack pack = dataGenerator.createPack();
+
+        pack.addProvider(HolidayServerPaintingVariantProvider::new);
+    }
+
+    @Override
+    public void buildRegistry(RegistryBuilder registryBuilder) {
+        registryBuilder.addRegistry(RegistryKeys.PAINTING_VARIANT, HolidayServerPaintingVariantProvider::register);
+    }
+}
